334e614d5d46cdf6cfe0debbfb97f7966fd71075
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-08-06 16:40:13 -0700

    transforming data with selectors

---
+++ src/helpers/selectors.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function getAppointmentsForDay(state, day) {
+
+  let days = state.days;
+  let appointments = state.appointments;
+  let selectedDay = [];
+  let appointment = [];
+
+  for (let i = 0; i < days.length; i++) {
+    if (days[i].name === day) {
+      selectedDay =(days[i].appointments)
+    }
+  }
+
+  for (let j = 0; j < selectedDay.length; j++) {
+    appointment.push(appointments[selectedDay[j]])
+  }
+  
+  return appointment;
+};
