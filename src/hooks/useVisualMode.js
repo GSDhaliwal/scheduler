06e9f73ac58d21a6a387ea3d37c8fece293ea487
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-08-07 13:00:23 -0700

    add transition, back, and back limit hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+
+  function transition(newMode) {
+    setMode(newMode)
+    setHistory([...history, newMode])
+  } 
+
+
+  function back() {
+
+    let backOneStep = (history.slice(-2))[0];
+    setMode(backOneStep)
+    
+    if (history.length > 1) {
+      history.pop();
+      setHistory(history)
+      }
+
+    }
+
+
+  return { mode, transition, back };
+}
