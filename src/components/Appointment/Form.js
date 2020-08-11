d326fb76a59f8b14261a88e5d9de073c5a43e536
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-08-10 17:39:36 -0700

    implement so student name is needed

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,6 +8,7 @@ export default function Form(props) {
 
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName("");
@@ -17,6 +18,15 @@ export default function Form(props) {
   const cancel = () => {
     reset()
     return props.onCancel()
+  }
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+  
+    props.onSave(name, interviewer);
   }
 
 
@@ -32,8 +42,10 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             onChange={(event) => setName(event.target.value)}
             value={name}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
           interviewers={props.interviewers} 
           value={interviewer} 
@@ -43,7 +55,8 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={() => cancel()}>Cancel</Button>
-          <Button onClick={() => props.onSave(name, interviewer)} confirm>Save</Button>
+          <Button onClick={
+            validate} confirm>Save</Button>
         </section>
       </section>
     </main>
